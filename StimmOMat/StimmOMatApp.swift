8e64ae4d95d2fba7cd4309f6ebfa69564a050cb8
Achim Friedland <achim.friedland@graphdefined.com>
2022-09-10 00:30:12 +0200

    Initial Commit

---
+++ StimmOMat/StimmOMatApp.swift
@@ -0,0 +1,17 @@
+//
+//  StimmOMatApp.swift
+//  StimmOMat
+//
+//  Created by Achim Friedland on 10.09.22.
+//
+
+import SwiftUI
+
+@main
+struct StimmOMatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
