fd1cbb27614e555025e85380d527987c62a23fb2
Achim Friedland <achim.friedland@graphdefined.com>
2022-09-10 19:44:11 +0200

    NavigationView for Nutzungsbedingungen/Datenschutz added

--- StimmOMat/ContentView.swift
+++ StimmOMat/ContentView.swift
@@ -13,140 +13,140 @@ struct ContentView: View {
     @State private var showButton = false
 
     var body: some View {
-
-        ZStack {
-
-            Image("mainScreen-background")
-                .resizable()
-                .scaledToFit()
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
             
-            VStack(alignment: .center,
-                   spacing:    0) {
-         
-                Image("mainScreen-top")
+            ZStack {
+
+                Image("mainScreen-background")
                     .resizable()
                     .scaledToFit()
-                    .padding(0)
-
-                Spacer()
-
-                Image("mainScreen-bottom")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(0)
-
-            }
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-
-            VStack(alignment: .center,
-                   spacing:    20) {
-         
-                Spacer()
-
-                HStack()
-                {
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 
-                    Button {
-                        print("Nutzungsbedingungen!")
-                    } label: {
-                        Text("Nutzungsbedingungen")
+                VStack(alignment: .center,
+                       spacing:    0) {
+             
+                    Image("mainScreen-top")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(0)
+
+                    Spacer()
+
+                    Image("mainScreen-bottom")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(0)
+
+                }
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+                VStack(alignment: .center,
+                       spacing:    20) {
+             
+                    Spacer()
+                    
+                    HStack()
+                    {
+
+                        NavigationLink(destination: NutzungsbedingungenView()) {
+                                       Text("Nutzungsbedingungen!")
+                                .font(.footnote)
+                                .foregroundColor(Color.white)
+                                    }
+
+                        Text("/")
                             .font(.footnote)
                             .foregroundColor(Color.white)
                             .multilineTextAlignment(.center)
-                    }
 
-                    Text("/")
-                        .font(.footnote)
+                        
+                        NavigationLink(destination: DatenschutzView()) {
+                                       Text("Datenschutz!")
+                                .font(.footnote)
+                                .foregroundColor(Color.white)
+                                    }
+                        
+                    }
+                    .padding(.bottom, 30.0)
+                        
+                }
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+                
+                VStack(alignment: .center,
+                       spacing:   0) {
+             
+                    Spacer()
+
+                    Text("StimmOMat")
+                        .font(.system(size: 66))
+                        .fontWeight(.semibold)
+                        .tracking(2)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
+                        .padding(.bottom, 15.0)
 
-                    Button {
-                        print("Datenschutz!")
-                    } label: {
-                        Text("Datenschutz")
-                            .font(.footnote)
-                            .foregroundColor(Color.white)
-                            .multilineTextAlignment(.center)
-                    }
-                    
-                }
-                .padding(.bottom, 30.0)
-                    
-            }
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    Text("Hallo, ich bin der StimmOmat.")
+                        .font(.system(size: 33))
+                        .fontWeight(.semibold)
+                        .tracking(2)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 30.0)
 
-            
-            VStack(alignment: .center,
-                   spacing:   0) {
-         
-                Spacer()
+                    Text("Ich stelle dir jetzt 3 Fragen\nund im Anschluss mache ich dein Bild.")
+                        .font(.system(size: 25))
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 50.0)
 
-                Text("StimmOMat")
-                    .font(.system(size: 66))
-                    .fontWeight(.semibold)
-                    .tracking(2)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 15.0)
+                    Text("Viel Spaß!")
+                        .font(.system(size: 25))
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 50.0)
 
-                Text("Hallo, ich bin der StimmOmat.")
-                    .font(.system(size: 33))
-                    .fontWeight(.semibold)
-                    .tracking(2)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 30.0)
-
-                Text("Ich stelle dir jetzt 3 Fragen\nund im Anschluss mache ich dein Bild.")
-                    .font(.system(size: 25))
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 50.0)
-
-                Text("Viel Spaß!")
-                    .font(.system(size: 25))
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 50.0)
-
-                Spacer()
-                    .frame(height: 30.0)
-                
-                if (showButton)
-                {
-                    Button{
-                        showButton.toggle()
-                        print("los gehts!")
-                    } label: {
-                        Text("Los geht's!")
-                            .foregroundColor(Color.white)
-                            .padding(.horizontal, 22)
-                            .padding(.vertical, 12.0)
-                            .background(Color(red: 224/255, green: 2/255, blue: 121/255))
-                            .font(.title)
-                    }
-                    .frame(height: 50)
-                }
-                else
-                {
                     Spacer()
+                        .frame(height: 30.0)
+
+                    if (showButton)
+                    {
+                        Button{
+                            showButton.toggle()
+                            print("los gehts!")
+                        } label: {
+                            Text("Los geht's!")
+                                .foregroundColor(Color.white)
+                                .padding(.horizontal, 22)
+                                .padding(.vertical, 12.0)
+                                .background(Color(red: 224/255, green: 2/255, blue: 121/255))
+                                .font(.title)
+                        }
                         .frame(height: 50)
+                    }
+                    else
+                    {
+                        Spacer()
+                            .frame(height: 50)
+                    }
+                    
+                    Spacer()
+
                 }
-                
-                Spacer()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 
             }
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .onTapGesture() {
+                print("view1 tapped!")
+                if (player.isPlaying == false) {
+                    player.playAudio(filename:   "stimmomat1_2_start",
+                                     onFinished: {_ in showButton = true })
+                }
+            }
 
         }
-        .onTapGesture() {
-            print("view1 tapped!")
-            if (player.isPlaying == false) {
-                player.playAudio(filename:   "stimmomat1_2_start",
-                                 onFinished: {_ in showButton = true })
-            }
-        }
+        .navigationViewStyle(StackNavigationViewStyle())
 
     }
 
---
+++ StimmOMat/NutzungsbedingungenView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  StimmOMat
+//
+//  Created by Achim Friedland on 10.09.22.
+//
+
+import SwiftUI
+
+struct NutzungsbedingungenView: View {
+    var body: some View {
+
+        ZStack {
+
+            Image("mainScreen-background")
+                .resizable()
+                .scaledToFit()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(alignment: .center,
+                   spacing:    0) {
+         
+                Image("mainScreen-top")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(0)
+
+                Spacer()
+
+                Image("mainScreen-bottom")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(0)
+
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(alignment: .center,
+                   spacing:   0) {
+         
+                Spacer()
+
+                Text("Nutzungsbedingungen")
+                    .font(.system(size: 44))
+                    .fontWeight(.semibold)
+                    .tracking(2)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30.0)
+
+                Text("Ich stelle dir jetzt 3 Fragen\nund im Anschluss mache ich dein Bild.")
+                    .font(.system(size: 25))
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 50.0)
+
+                Spacer()
+                
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+        }
+
+    }
+
+}
+
+struct NutzungsbedingungenView_Previews: PreviewProvider {
+    static var previews: some View {
+        NutzungsbedingungenView()
+    }
+}
