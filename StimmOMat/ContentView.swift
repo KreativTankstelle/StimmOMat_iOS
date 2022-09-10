e7e2719e7787e744b4f6a404fd6d6d18f6c58a31
Achim Friedland <achim.friedland@graphdefined.com>
2022-09-10 02:47:14 +0200

    Mainscreen is here

--- StimmOMat/ContentView.swift
+++ StimmOMat/ContentView.swift
@@ -8,10 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showButton = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        ZStack {
+
+            Image("mainScreen-background")
+                .resizable()
+                .scaledToFit()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(alignment: .center,
+                   spacing:    0) {
+         
+                Image("mainScreen-top")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(0)
+
+                Spacer()
+
+                Image("mainScreen-bottom")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(0)
+
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+            VStack(alignment: .center,
+                   spacing:    20) {
+         
+                Spacer()
+
+                Text("StimmOMat")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+
+                Text("Hallo, ich bin der StimmOmat.")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+
+                Text("Ich stelle dir jetzt 3 Fragen\nund im Anschluss mache ich dein Bild.")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+
+                Text("Viel Spaß!")
+                    .font(.title3)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                    .frame(height: 30.0)
+                
+                Button{
+                    showButton.toggle()
+                } label: {
+                    Text("Los geht's!")
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal, 22)
+                        .padding(.vertical, 12.0)
+                        .background(Color(red: 224/255, green: 2/255, blue: 121/255))
+                        .font(.title)
+                }
+
+                
+                
+                Spacer()
+
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+            VStack(alignment: .center,
+                   spacing:    20) {
+         
+                Spacer()
+
+                HStack()
+                {
+                
+                    Text("Nutzungsbedingungen")
+                        .font(.footnote)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+
+                    Text("/")
+                        .font(.footnote)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+
+                    Text("Datenschutz")
+                        .font(.footnote)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                    
+                }
+                .padding(.bottom, 30.0)
+                    
+            }
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+
+        }
+        
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
